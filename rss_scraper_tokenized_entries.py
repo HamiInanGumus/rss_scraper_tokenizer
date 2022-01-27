adbea8c7abbced6262c91daa3e615be149c46bb0
HamiInanGumus <98495231+HamiInanGumus@users.noreply.github.com>
2022-01-26 19:12:05 -0500

    Update rss_scraper_tokenized_entries.py

--- rss_scraper_tokenized_entries.py
+++ rss_scraper_tokenized_entries.py
@@ -7,7 +7,7 @@ import nltk, re, pprint
 from rss_scraper_tokenizedtext_class import *
 
 rss_tokenized_entries = RssTokenizedText.tokenize()
-result = print("Name of the tokenized entries list: 'rss_tokenized_entries' | \
+print("Name of the tokenized entries list: 'rss_tokenized_entries' | \
 use len(rss_tokenized_entries) to find the number of rss feed entries | \
 use rss_tokenized_entries[] or print(rss_tokenized_entries[]) with an index \
 number to see the content.")
